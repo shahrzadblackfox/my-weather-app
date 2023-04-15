24bb1b0222cb392841b1dbd3aef117654b76b29d
shahrzadblackfox <75183431+shahrzadblackfox@users.noreply.github.com>
2023-04-15 20:54:04 +0330

    getting response from api and create a set of data

--- src/Search.js
+++ src/Search.js
@@ -1,76 +1,71 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import "./Weather.css";
 
 export default function Weather() {
-  const apiKey = "4b44333bbcda5f0o6aaf4bt96ce9c0cd";
-  let [showMessage, setShowMessage] = useState("");
-  const [city, setCity] = useState("");
-  function handleSearch(event) {
-    event.preventDefault();
-    function showTempe(response) {
-      //console.log(response.data.main.temp);
-      let temp = Math.round(response.data.main.temp);
-      let dis = response.data.weather[0].description;
-      let humidity = response.data.main.humidity;
-      let wind = response.data.wind.speed;
-      let emoji = response.data.weather[0].icon;
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-      let info = [
-        { name: "Temperature", value: `${temp}°C` },
-        { name: "Description", value: `${dis}` },
-        { name: "Humidity", value: `${humidity}%` },
-        { name: "Wind", value: `${wind} km/h` },
-      ];
-      setShowMessage(
-        <ul>
-          {info.map((info) => (
-            <li key={info.name}>
-              {info.name}: {info.value}
-            </li>
-          ))}
-          <img
-            src={`https://openweathermap.org/img/wn/${emoji}@2x.png `}
-            alt="{info.name.Description}"
-          />
-        </ul>
-      );
-    }
-    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(url).then(showTempe);
+  const [city, setCity] = useState("");
+  function handleResponse(response) {
+    //console.log(response.data.main.temp);
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: Math.round(response.data.temperature.current),
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
   }
+
   function updateCity(event) {
     setCity(event.target.value);
   }
+  function search() {
+    const apiKey = "4b44333bbcda5f0o6aaf4bt96ce9c0cd";
+    let url = `https://api.shecodes.io/weather/v1/current?query=Tehran&key=${apiKey}&units=metric`;
+    axios.get(url).then(handleResponse);
+  }
+  function handleSearch(event) {
+    event.preventDefault();
+    search();
+  }
 
-  return (
-    <div>
-      <form onSubmit={handleSearch} className="row">
-        <div className="col-md-8">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Type a City"
-            onChange={updateCity}
-          />
-        </div>
-        <div className="col-md-2">
-          <input
-            type="submit"
-            value="Search"
-            className="sumbit-btn btn btn-outline-success w-100"
-          />
-        </div>
-        <div className="col-md-2">
-          <input
-            type="submit"
-            value="Current"
-            className="sumbit-btn btn btn-outline-success w-100"
-          />
-        </div>
-      </form>
-
-      <div className="weatherInfo">{showMessage}</div>
-    </div>
-  );
+  if (weatherData.ready) {
+    return (
+      <div>
+        <form onSubmit={handleSearch} className="row">
+          <div className="col-md-8">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Type a City"
+              onChange={updateCity}
+            />
+          </div>
+          <div className="col-md-2">
+            <input
+              type="submit"
+              value="Search"
+              className="sumbit-btn btn btn-outline-success w-100"
+            />
+          </div>
+          <div className="col-md-2">
+            <input
+              type="submit"
+              value="Current"
+              className="sumbit-btn btn btn-outline-success w-100"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
